33e5882da76f1a93b337c4b01662d131ff3d2c45
AmandaMS09 <mandanevs@gmail.com>
2024-08-22 12:49:01 -0300

    adding merged pull requests and issues count functions

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from datetime import date, datetime
+
+date_format = "%Y/%m/%d"
+
+today = datetime.__format__(date.today(), date_format)
+
 # Configuração do token de acesso pessoal
 token = os.getenv("GITHUB_TOKEN")
 
 # Função para obter os repositórios mais populares com a palavra-chave "microservices"
-def get_popular_repos(keyword, num_repos):
+def get_popular_repos_by_keyword(keyword, num_repos):
     url = f"https://api.github.com/search/repositories?q={keyword}&sort=stars&order=desc&per_page={num_repos}"
     headers = {"Authorization": f"token {token}"}
     response = requests.get(url, headers=headers)
@@ -82,6 +88,43 @@ def get_closed_issues(owner, repo):
             raise Exception(f"Failed to fetch closed issues: {response.status_code}")
     return len(closed_issues)
 
+# Função para obter a quantidade total de issues
+def get_issues_count(owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues?state=all"
+    headers = {"Authorization": f"token {token}"}
+    page = 1
+    issues = []
+    while True:
+        response = requests.get(f"{url}&page={page}&per_page=100", headers=headers)
+        if response.status_code == 200:
+            page_issues = response.json()
+            if not page_issues:
+                break
+            issues.extend(page_issues)
+            page += 1
+        else:
+            raise Exception(f"Failed to fetch issues: {response.status_code}")
+    return len(issues)
+
+# Função para obter o número de pull requests aceitas
+def get_merged_pull_requests_count(owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}/pulls?state=closed"
+    headers = {"Authorization": f"token {token}"}
+    page = 1
+    pull_requests = []
+    while True:
+        response = requests.get(f"{url}&page={page}&per_page=100", headers=headers)
+        if response.status_code == 200:
+            page_pull_requests = response.json()
+            if not page_pull_requests:
+                break
+            merged_pull_requests = filter(lambda x: x["merged_at"] != "null", page_pull_requests)
+            pull_requests.extend(merged_pull_requests)
+            page += 1
+        else:
+            raise Exception(f"Failed to fetch merged pull requests: {response.status_code}")
+    return len(pull_requests)
+
 # Função para coletar e imprimir informações dos repositórios
 def collect_and_print_repo_info(repos):
     for repo in repos:
@@ -112,12 +155,41 @@ def collect_and_print_repo_info(repos):
         print(f"Topics: {', '.join(repo_details['topics']) if 'topics' in repo_details else 'No topics'}")
         print("-" * 200)
 
+# Função para imprimir dados dos repositórios
+def print_repo_data(repos):
+    for repo in repos:
+        owner = repo["owner"]["login"]
+        repo_name = repo["name"]
+        repo_details = get_repo_details(owner, repo_name)
+        
+        merged_pull_requests = get_merged_pull_requests_count(owner, repo_name)
+        releases = get_releases(owner, repo_name)
+        closed_issues = get_closed_issues(owner, repo_name)
+        issues_count = get_issues_count(owner, repo_name)
+        treated_closed_issues = closed_issues if closed_issues is not None else 0
+        treated_issues_count = issues_count if issues_count is not None else 0
+
+        format_string = "%Y-%m-%dT%H:%M:%SZ"
+        creation_date = datetime.strptime(repo_details['created_at'], format_string)
+        age = datetime.strptime(today, date_format) - creation_date
+
+        print(f"Repository: {repo_name}")
+        print(f"Owner: {owner}")
+        print(f"Age: {age.days}")
+        print(f"Merged pull requests: {merged_pull_requests}")
+        print(f"Releases: {releases if releases is not None else 'N/A'}")
+        print(f"Last updated at: {datetime.strptime(repo_details['pushed_at'], format_string)}")
+        print(f"Main Language: {repo_details['language']}")
+        print(f"Closed Issues percentage: {"{:10.2f}".format(treated_closed_issues/treated_issues_count)}")
+        print("-" * 200)
+
 # Main
 if __name__ == "__main__":
     keyword = "microservices"
     num_repos = 10  # Número de repositórios a serem coletados
     try:
-        popular_repos = get_popular_repos(keyword, num_repos)
-        collect_and_print_repo_info(popular_repos)
+        popular_repos = get_popular_repos_by_keyword(keyword, num_repos)
+        # collect_and_print_repo_info(popular_repos)
+        print_repo_data(popular_repos)
     except Exception as e:
         print("ERROR: ",e)
